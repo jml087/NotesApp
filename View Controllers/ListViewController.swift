1b2e7218f0a76d93fc42fd7278d3658814e7ec62
Julia Li <jml087@ucsd.edu>
2018-11-17 13:09:06 -0600

    Added View Controllers to storyboard

---
+++ View Controllers/ListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ListViewController.swift
+//  Notes App
+//
+//  Created by Julia Molian Li on 11/16/18.
+//  Copyright © 2018 Julia Molian Li. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    @IBOutlet var notesTableView: UITableView!
+    var notes: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        notesTableView.delegate = self;
+        notesTableView.dataSource = self;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "note", for: indexPath) as! NoteTableViewCell
+        
+        cell.textLabel?.text = cell.title
+        
+        return cell
+    }
+    
+    
+}
---
+++ Views/NoteTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  NoteTableViewCell.swift
+//  Notes App
+//
+//  Created by Julia Molian Li on 11/16/18.
+//  Copyright © 2018 Julia Molian Li. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+    
+    var title : String = ""
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
